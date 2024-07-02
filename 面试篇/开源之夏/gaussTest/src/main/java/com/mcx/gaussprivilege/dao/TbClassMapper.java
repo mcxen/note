97362a3a465a4a3bff5c1736d21ce791f575a6f4
mcxw <imsalt@icloud.com>
2024-07-02 15:46:43 +0800

    update OSPP,Fix Redis DIR

--- 面试篇/开源之夏/gaussTest/src/main/java/com/mcx/gaussprivilege/dao/TbClassMapper.java
+++
@@ -1,9 +0,0 @@
-package com.mcx.gaussprivilege.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mcx.gaussprivilege.entity.TbClass;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TbClassMapper extends BaseMapper<TbClass> {
-}
